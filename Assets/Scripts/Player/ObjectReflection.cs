404d607fc3473d3f1884d6fdec8a6fc333abbd69
Matyy1975 <matias.gomeza@estudiantes.arcos.cl>
2023-06-08 12:46:39 -0400

    bug arreglado de reflejar ataques

--- Assets/Scripts/Player/ObjectReflection.cs
+++ Assets/Scripts/Player/ObjectReflection.cs
@@ -1,24 +1,24 @@
-using UnityEngine;
+    using UnityEngine;
 
-public class ObjectReflection : MonoBehaviour
-{
-    public float speed;
+    public class ObjectReflection : MonoBehaviour
+    {
+        public float speed;
     
-    private void OnCollisionEnter2D(Collision2D collision)
-    {    
-        if (collision.gameObject.CompareTag("Projectile"))
-        {
-            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            mousePosition.z = 0f;
+        private void OnCollisionEnter2D(Collision2D collision)
+        {    
+            if (collision.gameObject.CompareTag("Projectile"))
+            {
+                Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                mousePosition.z = 0f;
 
-            Vector3 direction = mousePosition - transform.position;
+                Vector3 direction = mousePosition - transform.position;
 
-            // Aplica la nueva dirección al objeto
-            // Puedes utilizar rb.velocity = reflectedDirection.normalized * speed; si el objeto tiene un Rigidbody2D y deseas aplicar una velocidad constante
-            // ransform.up = reflectedDirection.normalized;
+                // Aplica la nueva dirección al objeto
+                // Puedes utilizar rb.velocity = reflectedDirection.normalized * speed; si el objeto tiene un Rigidbody2D y deseas aplicar una velocidad constante
+                // ransform.up = reflectedDirection.normalized;
 
-            Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
-            rb.velocity = direction.normalized * speed;
+                Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
+                rb.velocity = direction.normalized * speed;
+            }
         }
     }
-}
