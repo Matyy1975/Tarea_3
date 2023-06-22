876580f270afe3b8a5c6b74759944dd54556c1db
Matyy1975 <matias.gomeza@estudiantes.arcos.cl>
2023-06-22 08:19:02 -0400

    Ahora Funciona el Colission al enemigo 2
    SEEEEE

--- Assets/Scripts/Enemy/HitCollision.cs
+++ Assets/Scripts/Enemy/HitCollision.cs
@@ -6,7 +6,18 @@ public class HitCollision : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        
+        bool collisionWithReflect = false;
+        int cc = collision.transform.childCount;
+        for (int i = 0; i < cc; i++)
+        {
+            if(collision.transform.GetChild(i).name == "ReflectMelee" &&
+                collision.transform.GetChild(i).gameObject.activeInHierarchy)
+            {
+                collisionWithReflect = true;
+            }
+        }
+        if (collisionWithReflect == false)
         {
             PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
             if (playerController != null)
@@ -14,12 +25,20 @@ public class HitCollision : MonoBehaviour
                 playerController.TakeDamage(damage);
             }
         }
-        else if (collision.gameObject.CompareTag("Melee"))
+        else
         {
-            EnemyHealth enemyHealth = collision.gameObject.GetComponent<EnemyHealth>();
+            EnemyHealth enemyHealth = this.gameObject.GetComponent<EnemyHealth>();
             if (enemyHealth != null)
             {
                 enemyHealth.TakeDamage(damage);
+            }
+            else
+            {
+                enemyHealth = this.transform.parent.GetComponent<EnemyHealth>();
+                if (enemyHealth != null)
+                {
+                    enemyHealth.TakeDamage(damage);
+                }
             }
         }
     }
