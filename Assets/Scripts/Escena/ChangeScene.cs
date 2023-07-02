68f4643da1a338bee53d03d4c4143b226e58be6a
Matyy1975 <132588328+Matyy1975@users.noreply.github.com>
2023-07-01 23:49:44 -0400

    Transition de cambio de escena ya agregados!!!
    y se modifico un poco mas la interfaz

--- Assets/Scripts/Escena/ChangeScene.cs
+++ Assets/Scripts/Escena/ChangeScene.cs
@@ -5,13 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+    private Animator animator; // Referencia al componente Animator
+    private bool isTransitioning = false; // Indica si se está reproduciendo una transición
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     public string CambioEscena; // Nombre de la escena a la que se cambiará
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && !isTransitioning)
         {
-            SceneManager.LoadScene(CambioEscena); // Cambiar a la escena especificada
+            isTransitioning = true; // Marcar que se está reproduciendo una transición
+
+            // Reproducir la animación de transición
+            animator.SetTrigger("Transition");
+
+            // Esperar la duración de la animación de transición antes de cambiar la escena
+            float transitionDuration = animator.GetCurrentAnimatorStateInfo(0).length;
+            StartCoroutine(ChangeSceneAfterDelay(transitionDuration));
         }
     }
+
+    private IEnumerator ChangeSceneAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        // Cambiar a la escena especificada
+        SceneManager.LoadScene(CambioEscena);
+    }
+
 }
