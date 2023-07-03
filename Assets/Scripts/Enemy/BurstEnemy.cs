f473bbab19ca5c23abcdb227024ddab2a7e4c0de
Matyy1975 <132588328+Matyy1975@users.noreply.github.com>
2023-07-03 01:17:16 -0400

    Enemigo 4 Ataque Listooooo, ahora falta el ultimo jeje

--- Assets/Scripts/Enemy/BurstEnemy.cs
+++ Assets/Scripts/Enemy/BurstEnemy.cs
@@ -8,9 +8,11 @@ public class BurstEnemy : MonoBehaviour
     public float projectileSpeed = 10f; // Velocidad del proyectil
     public float burstInterval = 2f; // Intervalo entre ráfagas
     public int burstCount = 3; // Cantidad de proyectiles en cada ráfaga
+    public float shootingPointOffset = 1f; // Desplazamiento del punto de disparo
 
     private GameObject player; // Referencia al jugador
     private float nextBurstTime; // Tiempo para la próxima ráfaga
+    private EnemyHealth enemyHealth; // Referencia al script EnemyHealth
 
     private void Start()
     {
@@ -22,6 +24,7 @@ public class BurstEnemy : MonoBehaviour
         }
 
         nextBurstTime = Time.time + burstInterval; // Establecer el tiempo para la próxima ráfaga
+        enemyHealth = GetComponent<EnemyHealth>(); // Obtener referencia al script EnemyHealth
     }
 
     private void Update()
@@ -38,8 +41,21 @@ public class BurstEnemy : MonoBehaviour
             Vector3 direction = player.transform.position - shootingPoint.position;
             Quaternion rotation = Quaternion.LookRotation(Vector3.forward, direction);
 
+            // Desactivar el collider del enemigo temporalmente
+            Collider2D enemyCollider = GetComponent<Collider2D>();
+            if (enemyCollider != null)
+            {
+                enemyCollider.enabled = false;
+            }
+
             // Disparar ráfaga de proyectiles hacia la posición del jugador
             StartCoroutine(PerformBurst(direction.normalized, rotation));
+
+            // Activar el collider del enemigo nuevamente
+            if (enemyCollider != null)
+            {
+                enemyCollider.enabled = true;
+            }
 
             nextBurstTime = Time.time + burstInterval; // Actualizar el tiempo para la próxima ráfaga
         }
@@ -49,12 +65,28 @@ public class BurstEnemy : MonoBehaviour
     {
         for (int i = 0; i < burstCount; i++)
         {
+            // Desplazar el punto de disparo hacia afuera del objeto
+            Vector3 shootingPointPosition = shootingPoint.position + direction * shootingPointOffset;
+
             // Instanciar el proyectil en el punto de disparo
-            GameObject projectile = Instantiate(projectilePrefab, shootingPoint.position, rotation);
+            GameObject projectile = Instantiate(projectilePrefab, shootingPointPosition, rotation);
             Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
             rb.velocity = direction * projectileSpeed;
 
             yield return new WaitForSeconds(0.1f); // Esperar un breve intervalo entre cada proyectil de la ráfaga
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Projectile"))
+        {
+            // Aplicar daño al enemigo
+            if (enemyHealth != null)
+            {
+                enemyHealth.TakeDamage(1); // Llamar al método TakeDamage del script EnemyHealth con el valor de daño deseado
+            }
+            Destroy(collision.gameObject);
         }
     }
 }
