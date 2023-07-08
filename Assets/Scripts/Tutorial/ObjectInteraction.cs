8e0954aa295703e41b5416ad862916d27e86bf97
MATU <132588328+Matyy1975@users.noreply.github.com>
2023-07-08 01:30:20 -0400

    Revert "Vamos a hacer unos cambios a partir de aqui"
    This reverts commit 5137936aa4aea2cc08fe567221c0faf244b9311d.

--- Assets/Scripts/Tutorial/ObjectInteraction.cs
+++ Assets/Scripts/Tutorial/ObjectInteraction.cs
@@ -5,40 +5,42 @@ public class ObjectInteraction : MonoBehaviour
     public GameObject projectilePrefab; // Prefab del proyectil
     public Transform shootingPoint; // Punto de disparo
     public float projectileSpeed = 10f; // Velocidad del proyectil
-    public float shootingInterval = 2f; // Intervalo entre disparos
+    public KeyCode interactionKey = KeyCode.E; // Tecla de interacción
 
-    private float shootingTimer; // Temporizador para controlar el intervalo entre disparos
+    private PlayerController playerController;
 
     private void Start()
     {
-        shootingTimer = shootingInterval; // Inicializar el temporizador con el intervalo de disparo
+        playerController = FindObjectOfType<PlayerController>();
+
+        if (playerController == null)
+        {
+            Debug.LogError("PlayerController component not found in the scene!");
+        }
     }
 
     private void Update()
     {
-        // Actualizar el temporizador
-        shootingTimer -= Time.deltaTime;
-
-        // Verificar si ha pasado el tiempo suficiente para disparar
-        if (shootingTimer <= 0f)
+        // Verificar si el jugador presiona la tecla de interacción
+        if (Input.GetKeyDown(interactionKey))
         {
-            // Reiniciar el temporizador
-            shootingTimer = shootingInterval;
+            // Obtener la dirección hacia el jugador
+            Vector3 direction = (playerController.transform.position - shootingPoint.position).normalized;
 
-            // Disparar el proyectil
-            ShootProjectile();
+            // Disparar el proyectil en la dirección calculada
+            ShootProjectile(direction);
         }
     }
 
-    private void ShootProjectile()
+    private void ShootProjectile(Vector3 direction)
     {
         // Crear el proyectil
-        GameObject projectile = Instantiate(projectilePrefab, shootingPoint.position, shootingPoint.rotation);
+        GameObject projectile = Instantiate(projectilePrefab, shootingPoint.position, Quaternion.identity);
 
         // Obtener el componente Rigidbody2D del proyectil
         Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
 
         // Aplicar la velocidad en la dirección especificada
-        rb.velocity = shootingPoint.right * projectileSpeed;
+        rb.velocity = direction * projectileSpeed;
     }
 }
