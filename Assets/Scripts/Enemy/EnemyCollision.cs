a11b83e5faa5ae172b008374325411851d84385e
MATU <132588328+Matyy1975@users.noreply.github.com>
2023-07-22 20:46:33 -0400

    Animacion de hit enemigo listo

--- Assets/Scripts/Enemy/EnemyCollision.cs
+++ Assets/Scripts/Enemy/EnemyCollision.cs
@@ -15,12 +15,16 @@ public class EnemyCollision : MonoBehaviour
     // OnCollisionEnter2D is called when the attached Collider2D component collides with another Collider2D
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        //ANIMACION DE DAÑO
+        //ENEMY1
         // Check if the collided object has the "Projectile" tag
-        if (collision.collider.CompareTag("Projectile"))
+        if (collision.collider.CompareTag("Projectile") || collision.collider.CompareTag("Projectile 2") || collision.collider.CompareTag("Projectile 3")|| collision.collider.CompareTag("Training Projectile"))
         {
             // Set the "Injunemy" trigger to true, activating the animation state
             animator.SetTrigger("Injunemy");
         }
+
+
         if (collision.collider.CompareTag("Projectile 3"))
         {
             // Set the "Injunemy" trigger to true, activating the animation state
